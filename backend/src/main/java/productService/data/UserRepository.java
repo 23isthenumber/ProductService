efbe39f703daf875a9fc9eb7ec80d186cc91a496
23isthenumber <58682670+23isthenumber@users.noreply.github.com>
2020-10-12 10:19:12 +0200

    sec del
    working on security

--- backend/src/main/java/productService/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package productService.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import productService.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserName(String userName);
-}
